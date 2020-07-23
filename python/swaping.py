3e4bb9b7c417140c26839010618c80093aa08f3b
Damayanti2911 <68653915+Damayanti2911@users.noreply.github.com>
2020-07-23 17:19:47 +0530

    Create swaping.py

---
+++ python/swaping.py
@@ -0,0 +1,18 @@
+#2 variables
+s = int(input('Enter a no:'))
+g = int(input('Enter a no:'))
+x = s 
+s = g 
+g  = x
+print('swaped no are:',s,g)
+
+#three variables
+p = int(input('Enter a no:'))
+q = int(input('Enter a no:'))
+r = int(input('Enter a no:'))
+text = p 
+p= q 
+q = r  
+r = text 
+print('Swapped nos are: ',p,q,r )
+
