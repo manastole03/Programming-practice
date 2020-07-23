bdc7ef75dfe2cc082e68717c7862c9e32dc9599a
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-23 17:10:49 +0530

    Create oddeven.py

---
+++ python/odd or even.py/oddeven.py
@@ -0,0 +1,8 @@
+num=int(input('enter any number : '))
+for i in range(2,num):
+    if num%2==0:
+        print('the number is even')
+        break
+    else:
+        print('the number is odd')
+        break
