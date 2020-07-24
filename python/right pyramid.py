2d22a0923dc5e7d24cb859c3a93e19fff1573ecf
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-24 20:44:05 +0530

    Create right pyramid.py

---
+++ python/right pyramid.py
@@ -0,0 +1,14 @@
+# To create a right star pyramid (*)
+
+def pattern(n):
+    k = 2 * n - 2
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 2
+        for j in range(0, i + 1):
+            print("* ", end="")
+        print("\r")
+
+n=int(input('enter the number of rows : '))
+pattern(n)
