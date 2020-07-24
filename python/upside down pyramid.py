9ad3793b559756df2c91e1bd20e493af25740385
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-24 20:46:04 +0530

    Create upside down pyramid.py

---
+++ python/upside down pyramid.py
@@ -0,0 +1,13 @@
+# To form an upside down star(*) pyramid
+
+def pattern(n):
+    k = 2 * n - 2
+    for i in range(n, -1, -1):
+        for j in range(k, 0, -1):
+            print(end=" ")
+        k = k + 1
+        for j in range(0, i + 1):
+            print("*", end=" ")
+        print("\r")
+n=int(input('enter the number of rows : '))
+pattern(n)
