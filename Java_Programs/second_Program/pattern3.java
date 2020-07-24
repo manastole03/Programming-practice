9bfca09fe9981e61c1a169a65391e8e27864c6f3
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-07-24 19:55:02 +0530

    Create pattern3.java

---
+++ Java_Programs/second_Program/pattern3.java
@@ -0,0 +1,29 @@
+package introduction;
+import java.util.Scanner;
+public class pattern3 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter a number ");
+		int n=sc.nextInt();
+		for (int i=1;i<=n;i++) {
+			for(int j=1;j<=n-i;j++) {
+			System.out.print("  ");
+			}
+		for (int j=1;j<=1;j++) {
+			System.out.print("*");
+		};
+			System.out.println();
+		}
+		}
+}
+//expected output:
+//enter a number 4
+//      *
+//    *
+//  *
+//*
+
+
+
+
