357b3df68ae5f931c93f6a590d6ae1d97cdbd786
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-24 20:36:38 +0530

    Create left pyramid.py

---
+++ python/left pyramid.py
@@ -0,0 +1,13 @@
+# To create left half pyramid (*)
+
+def pattern(n):
+    k = 2 * n - 2
+    for i in range(0, n):
+
+        k = k - 1
+        for j in range(0, i + 1):
+            print("*", end=" ")
+        print("\r")
+
+rows=int(input('enter the number of rows : '))
+pattern(rows)
