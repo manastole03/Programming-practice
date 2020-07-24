ca40aea5751c98346da020683118363a08c823c3
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-24 20:41:33 +0530

    Update left pyramid.py

--- python/left pyramid.py
+++ python/left pyramid.py
@@ -1,13 +1,10 @@
 # To create left half star pyramid (*)
 
 def pattern(n):
-    k = 2 * n - 2
     for i in range(0, n):
-
-        k = k - 1
         for j in range(0, i + 1):
-            print("*", end=" ")
+            print("* ", end="")
         print("\r")
 
-rows=int(input('enter the number of rows : '))
-pattern(rows)
+n=int(input('enter the number of rows : '))
+pattern(n)
