f91cd2d336fe0127b2576f16a779e260bc86675e
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-25 21:56:30 +0530

    Create priority check.py

---
+++ python/priority check.py
@@ -0,0 +1,17 @@
+# To check the priority for the mathematical operators used in Python 
+# Debug the program for better understanding
+
+a=4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+print(a)
+
+a=((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+print(a)
+
+
+# To find the perimeter of the circle
+r=input('enter the radius in cm : ')
+radius=float(r)
+π=3.14159265359
+perimeter=2*π*radius
+print('the perimeter of circle is : ',perimeter)
+
