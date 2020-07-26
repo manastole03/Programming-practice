c625f783021bf4f817bf71db117789943b45fbaf
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-07-26 23:17:04 +0530

    Create insertion.java

---
+++ Java_Programs/second_Program/insertion.java
@@ -0,0 +1,10 @@
+package introduction;
+
+public class insert {
+
+	public static void main(String[] args) {
+	String actual="Python 3.0 ";
+	String add="Tutorial";
+	System.out.println(actual.substring(0,7)+add+actual.substring(6));
+	}
+}
