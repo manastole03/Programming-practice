f6346209442bed1a42d52d410d92baf32efe5806
Yashashree125 <68264499+Yashashree125@users.noreply.github.com>
2020-07-26 10:23:26 +0530

    Create Display_large_no.cpp

---
+++ Cpp_Programs/10./Display_large_no.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int first_no,second_no;
+	cout<<"Input First number ";
+	cin>>first_no;
+	cout<<"Input Second number ";
+	cin>>second_no;
+	
+	if (first_no>second_no){
+		cout<<"Result "<<first_no;
+	}
+	else
+	cout<<"Result "<<second_no;
+  
+  
+  return 0;
+	
+}
+	
