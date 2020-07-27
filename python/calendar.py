354fc5d3520b96c9421d02bc1d8532d025d2341d
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-27 21:55:29 +0530

    Update calendar.py

--- python/calendar.py
+++ python/calendar.py
@@ -1,21 +1,8 @@
-# To print the calendar of given year and month
+# To get the calendar of the specific month and year from the user input
 
 import calendar
-while True:
-    y = int(input("Input the year (YYYY) : "))
-    year_length=len(str(y))
-    if year_length>4:
-        print('enter appropriate year : ')
-    elif year_length==4:
-         break
-while True:
-    try:
-        while True:
-            m = int(input('Input the month (MM) : '))
-            month_length=len(str(m))
-            if month_length>2:
-                print('enter appropriate month ')
-            else:
-                print(calendar.month(y, m))
-    except IndexError:
-        print('no such month exists\nplease enter appropriate month below')
+
+y = int(input("Input the year (YYYY) : "))
+m = int(input('Input the month (MM) : '))
+
+print(calendar.month(y,m))
