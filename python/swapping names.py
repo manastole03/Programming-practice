083f76f095fee66510cef34e4669961675a00bf0
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-27 14:35:42 +0530

    Create swapping names.py

---
+++ python/swapping names.py
@@ -0,0 +1,8 @@
+# Swapping the first name and last name
+
+first_name=str(input('enter your first name : '))
+last_name=str(input('enter your last name : '))
+a=first_name
+first_name=last_name
+last_name=a
+print('the names after swapping :'+first_name+' ',last_name)
