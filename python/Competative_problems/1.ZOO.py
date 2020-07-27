055a60c06f33f176d97a96ccd5a948ffec908256
Damayanti2911 <68653915+Damayanti2911@users.noreply.github.com>
2020-07-27 20:13:01 +0530

    Create 1.ZOO.py

---
+++ python/Competative_problems/1.ZOO.py
@@ -0,0 +1,14 @@
+a = input('Enter a character which contains two letters : ')
+b = a[0]
+c = a[-1]
+count = 0
+max1 = 0
+for i in range(0,len(a)):
+    if b == a[i]:
+        count +=1
+    if c == a[i]:
+        max1 +=1
+if count*2 ==max1:
+    print('YES!')
+else:
+    print('NO!')
