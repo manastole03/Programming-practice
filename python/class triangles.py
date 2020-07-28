d3833cfdca61f0641e96a1a568cc3cd508850c9e
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-28 22:16:48 +0530

    Create class triangles.py

---
+++ python/class triangles.py
@@ -0,0 +1,20 @@
+class triangle:
+    def perimeter():
+       a=3
+       b=4
+       c=5
+       formula_for_perimeter_of_triangle=a+b+c
+       b=str(formula_for_perimeter_of_triangle)
+       print('the perimeter of triangle is : '+b+' units')
+
+    def area():
+        base=4
+        height=5
+        formula_for_area_of_triangle=0.5*base*height
+        a=str(formula_for_area_of_triangle)
+        print('the area of triangle is : '+a+' units')
+
+    perimeter()
+    area()
+
+
