e71ac2f68e9a03f7420d4f737793a194fffc048e
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-07-28 22:15:59 +0530

    Create class students(2).py

---
+++ python/class students(2).py
@@ -0,0 +1,11 @@
+class students:
+    def __init__(self,name,phone_number,address):
+        self.name=name
+        self.phone_number=phone_number
+        self.address=address
+        print('the first students info is : \nname:'+name+'\nmobile number is : '+phone_number+'\nand address is :',address +'\n\n')
+
+myclass=students('sam','9374654328','india')
+myclass1=students('john','9984584328','india')
+
+
