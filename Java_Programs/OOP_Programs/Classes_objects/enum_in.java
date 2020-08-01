0be46b55758e73a588665ae3e0f35fdf158abf85
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-01 22:47:11 +0530

    Create enum_in.java

---
+++ Java_Programs/OOP_Programs/Classes_objects/enum_in.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+enum Name{
+	robert,alex,albert,tom,sam;
+}
+public class enum_in {
+  public static void main(String[] args) {
+		Name n1= Name.alex;
+		System.out.println(n1);
+	}
+}
