33ea376660e25f4589945b0e3202ee688fcb45b4
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-03 23:43:36 +0530

    Create Array2.java

---
+++ Java_Programs/Array_complex/Array2.java
@@ -0,0 +1,21 @@
+package myAllJavaProjects;
+import java.util.*;
+import java.io.*;
+
+public class Array6
+{
+
+	public static void main(String[] args) 
+	{
+		int[] array = {1,2,3,4,5,6,7,8,9,0};
+		int sum = 0;
+		for(int i : array)
+		{
+			 sum = sum + i ;
+		}
+		System.out.print("Sum of the array :" + sum); 
+		
+
+	}
+
+}
