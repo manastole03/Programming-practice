a21cf3f93695ea660946d0328786cc4cebdd2912
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-03 23:44:17 +0530

    Create Array3.java

---
+++ Java_Programs/Array_complex/Array3.java
@@ -0,0 +1,21 @@
+package myAllJavaProjects;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Array7 
+{
+
+	public static void main(String[] args) 
+	{
+		int[][] arr = new int[10][10];
+		
+		for(int i=0 ; i<10 ; i++)
+		{
+			for(int j=0 ; j<10 ; j++)
+			{
+				System.out.printf("%2d ", arr[i][j]);			}
+		}
+		System.out.print("");
+	}
+
+}
