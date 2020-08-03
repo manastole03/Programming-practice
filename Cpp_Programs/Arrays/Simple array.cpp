defc2a28962291bd9e9548819e46e8d0f9e08a84
Yashashree125 <68264499+Yashashree125@users.noreply.github.com>
2020-08-03 18:34:55 +0530

    Create Simple array.cpp

---
+++ Cpp_Programs/Arrays/Simple array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+	int A[3][3]={{1,2,3},{7,8,9},{5,6,4}};
+	for (int i=0;i<3;i++)
+	{
+		for (int j=0;j<3;j++)
+		{
+			cout<<A[i] [j];
+		}
+	}
+	cout << endl; 
+return 0;
+}
