e5602297a186abe21d7259ee0e2d1b92b5cd4e01
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-08-07 00:23:41 +0530

    Create average array.py

---
+++ python/Array/average array.py
@@ -0,0 +1,15 @@
+#Write a python program to calculate the average value of array elements. 
+
+
+num=int(input('how many numbers you want: '))
+list=[]
+for i in range(num):
+    enter=int(input('enter the number: '))
+    list.append(enter)
+
+avg_list=sum(list)/len(list)
+
+print('the array is: ',list)
+print('the average of elements are: ',float(avg_list))
+
+
