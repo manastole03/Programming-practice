533f1a5b14b2c9332ba0b4cd860d06bb09de1a16
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-06 22:27:58 +0530

    Update sorting_numbers.java

--- Java_Programs/Array/sorting_numbers.java
+++ Java_Programs/Array/sorting_numbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.Scanner;
-public class recu {
+public class sorting_num {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("enter any number: ");
