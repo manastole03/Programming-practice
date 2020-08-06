f155d128982a065caa74a8eb4972b15df7de5485
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-06 22:38:31 +0530

    Create dashGrid.java

---
+++ Java_Programs/Array/dashGrid.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Scanner;
+public class dash_grid {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter any number: ");
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("Printing output...");
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print("-");
+			}System.out.println();
+		}
+	}
+}
+//expected output:
+//enter any number: 3
+//Printing output...
+//---
+//---
+//---
