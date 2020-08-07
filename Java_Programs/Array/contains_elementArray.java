94939ccd470017a5c1415fc03b9221231d13742f
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-07 23:23:09 +0530

    Update contains_elementArray.java

--- Java_Programs/Array/contains_elementArray.java
+++ Java_Programs/Array/contains_elementArray.java
@@ -1,29 +1,30 @@
 import java.util.*;
 import java.util.Scanner;
-public class contains_element {
+public class contains_elements {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("enter any number: ");
 		int n=sc.nextInt();
-		char a[]=new char[n];
+		String a[]=new String[n];
 		System.out.println("Enter array string or integer: ");
 		for(int i=0;i<n;i++) {
-			a[i]=sc.next().charAt(0);
+			a[i]=sc.next();
 		}
 		System.out.println("Enter a element: ");
-		char m=sc.next().charAt(0);
-		boolean contains = false;
-		for (char c : a) {
-		    if (c == m) {
-		        contains = true;
-		        break;
+		String m=sc.next();
+		int contains = 0;
+		for(int i=0;i<n;i++) {
+		    if (a[i].equals(m)) {
+		    	contains=1;
 		    }
 		}
-		if (!contains) {
-			System.out.println("NO given input is not in array");
-		}
-		if(contains==true) {
-			System.out.println("YES!! given input is in array");
-		}
+		    switch(contains) {
+		    case 1:
+		    	System.out.println("YES!! element is in array");
+		    	break;
+		    default:
+		    	System.out.println("NO element is not in array");
+		    }
+		    System.out.println("Input array is: "+Arrays.toString(a));
 	}
 }
