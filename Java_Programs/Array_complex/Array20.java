81e8014f4407f6b4e84711d1d6d239017f72dd49
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-07 23:14:25 +0530

    Create Array20.java

---
+++ Java_Programs/Array_complex/Array20.java
@@ -0,0 +1,14 @@
+package myAllJavaProjects;
+import java.io.*;
+import java.util.*;
+
+public class Array24 
+{
+	public static void main(String[] args)
+	{
+		  String[]  my_array = new String[] {"Python", "JAVA", "PHP",  "Perl", "C#", "C++"};
+		  ArrayList<String>  list = new ArrayList<String>(Arrays.asList(my_array));
+		  
+		  System.out.println(list);
+	}
+}
