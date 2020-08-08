a5fd8d79d21edb8b289d97c53d571a74164ae30e
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-08 20:06:36 +0530

    Update arrayToArrayList.java

--- Java_Programs/Array/arrayToArrayList.java
+++ Java_Programs/Array/arrayToArrayList.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 public class arrayToArraylist {
 
