d891736063e586734e2c343368932a50fa86d02c
Damayanti2911 <68653915+Damayanti2911@users.noreply.github.com>
2020-08-11 12:48:33 +0530

    Create Avg_exclude_large_small_values.py

---
+++ python/Array/Avg_exclude_large_small_values.py
@@ -0,0 +1,15 @@
+array=[]
+n=int(input('How many elements u want in array: '))
+for i in range(n):
+    f= int(input('Enter no: '))
+    array.append(f)
+print('Enteredarray: ',array)
+mx=max(array)
+mn=min(array)
+array.remove(mn)
+array.remove(mx)
+sum1=0
+for i in array:
+    sum1 +=i
+avg=sum1/len(array)
+print('Average of array after excluding large & small value: ',avg)
