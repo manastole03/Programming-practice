145cbcda73fddf5492ac5c6481154f5f29ba3930
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-12 23:09:29 +0530

    Create String9.java

---
+++ Java_Programs/String_Programs1/String9.java
@@ -0,0 +1,15 @@
+package myAllJavaProjects;
+
+public class String9 
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Manas";
+		CharSequence ch = "Manas";
+		
+		System.out.print("Comparing :" + str.contentEquals(ch));
+
+	}
+
+}
