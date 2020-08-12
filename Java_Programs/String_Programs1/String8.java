f0baceb9422617cecaf0729f0bfd4576e9b1fab3
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-12 23:08:53 +0530

    Create String8.java

---
+++ Java_Programs/String_Programs1/String8.java
@@ -0,0 +1,15 @@
+package myAllJavaProjects;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class String8 
+{
+	public static void main(String[] args)
+	{
+		String str1 = "Manas";
+		String str2 = "Manas";
+		
+		System.out.print("Contains :" + str1.contains(str2));
+	}
+}
