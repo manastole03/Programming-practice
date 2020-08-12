ff063e93df7307098d4608a8301281c6c4392507
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-12 23:07:11 +0530

    Create String7.java

---
+++ Java_Programs/String_Programs1/String7.java
@@ -0,0 +1,16 @@
+package myAllJavaProjects;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class String7
+{
+	public static void main(String[] args)
+	{
+		String str1 = "Manas";
+		String str2 = " Tole";
+		
+		String str3 = str1.concat(str2);
+		System.out.print(str3);
+	}
+}
