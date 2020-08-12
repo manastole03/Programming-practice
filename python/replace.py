bfb3a98aa024a392a487b10cdc3b9538ec84216d
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2020-08-12 17:48:00 +0530

    Create replace.py

---
+++ python/replace.py
@@ -0,0 +1,8 @@
+# Write a Python program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself. 
+
+
+inp=input('enter the string: ')
+char = inp[0]
+inp = inp.replace(char, '$')
+inp = char + inp[1:]
+print(inp)
