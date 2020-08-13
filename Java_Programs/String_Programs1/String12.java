49fcb5968a36774a3be2615235f71469909a8b93
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-13 22:24:23 +0530

    Create String12.java

---
+++ Java_Programs/String_Programs1/String12.java
@@ -0,0 +1,17 @@
+package myAllJavaProjects;
+
+public class String12
+{
+	public static void main(String[] args)
+	{
+		String str1 = "manas";
+		String str2 = "nas";
+		
+		String my = "anas";
+		
+		boolean hi1 = str1.endsWith(my);
+		boolean hi2 = str2.endsWith(my);
+		
+		System.out.print(hi1 + "," + hi2);
+	}
+}
