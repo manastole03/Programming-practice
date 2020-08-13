f4e23892727bf2aca03178baf2fa76b86ac05b3c
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-13 14:33:02 +0530

    Create largestandSmallestDiff.java

---
+++ Java_Programs/Array/largestandSmallestDiff.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.Scanner;
+public class diffInLargestAndSmallest {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of matrix: ");
+		int n=sc.nextInt();
+		System.out.println("Enter elements of first matrix: ");
+		int a[]=new int[n];
+		
+		for (int i=0;i<n;i++ ) {
+			a[i]=sc.nextInt();
+		}
+		System.out.println("original array: "+Arrays.toString(a));
+		int diff=0;
+		for(int i=0;i<n;i++) {
+			Arrays.parallelSort(a);
+			 diff=a[n-1]-a[0];
+		}
+    System.out.println("difference between largest and smallest element is: "+diff);
+  }
+}
