d47e2e283af4399add35259ff0714013f46cfa87
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-14 19:59:56 +0530

    Create String3.java

---
+++ Java_Programs/String_programs_2/String3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class elementPresent {
+
+	public static void main(String[] args) {
+		String a="Umbrella";
+		System.out.print("The word 'e' is present in "+a+" : " );
+		System.out.print(a.contains("e"));
+	}
+}
