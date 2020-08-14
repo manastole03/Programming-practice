f9abf4e7b95e1d0ea4a8fea8056390e7a2093e2b
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-14 19:56:52 +0530

    Create String1.java

---
+++ Java_Programs/String_programs_2/String1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class inputFromUser {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String: ");
+		String a=sc.next();
+		System.out.println("Entered string is: "+a);
+	}
+}
