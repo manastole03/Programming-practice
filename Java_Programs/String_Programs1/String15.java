e8913b253f5aa1f576d90160d9e92f30fb06dd83
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-14 23:43:15 +0530

    Create String15.java

---
+++ Java_Programs/String_Programs1/String15.java
@@ -0,0 +1,13 @@
+package myAllJavaProjects;
+import java.util.*;
+public class String15
+{
+	public static void main(String[] args)
+	{
+		 Calendar c = Calendar.getInstance();
+	      System.out.println("Current Date and Time :"); 
+	      System.out.format("%tB %te, %tY%n", c, c, c);
+	      System.out.format("%tl:%tM %tp%n", c, c, c); 
+		
+	}
+}
