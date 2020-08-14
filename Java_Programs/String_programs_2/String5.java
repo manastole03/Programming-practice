5d7057ceddc47c6d058d17d0e98b62067fa61f3f
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-14 20:01:30 +0530

    Create String5.java

---
+++ Java_Programs/String_programs_2/String5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class indexString{
+	public static void main(String[] args) {
+		String a="Hello,World";
+		System.out.print("The index of word 'o' in ("+a+") : " );
+		System.out.println(a.indexOf("o"));
+		System.out.print("The index of word ',' in ("+a+") : ");
+		System.out.println(a.indexOf(","));
+		}
+}
