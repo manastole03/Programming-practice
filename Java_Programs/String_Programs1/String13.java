12d2d39fe3c6863018b7f1b628d0d0174bd02400
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-14 23:42:02 +0530

    Create String13.java

---
+++ Java_Programs/String_Programs1/String13.java
@@ -0,0 +1,18 @@
+package myAllJavaProjects;
+
+public class String13 
+{
+	public static void main(String[] args)
+	{
+		String str1 = "Manas";
+		String str2 = "Tole";
+		String str3 = "here";
+		String str4 = "here";
+		
+		boolean my1 = str1.equals(str2);
+		boolean my2 = str3.equals(str4);
+		
+		System.out.print("str1 == str2 :" + my1);
+		System.out.print("\nstr3 == str4 :" + my2);
+	}
+}
