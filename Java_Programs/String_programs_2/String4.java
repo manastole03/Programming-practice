c0cab44cf6100e365acb5f967967db8320599aff
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-14 20:00:47 +0530

    Create String4.java

---
+++ Java_Programs/String_programs_2/String4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+public class wordPresent {
+
+	public static void main(String[] args) {
+		String a="This is orange juice";
+		System.out.print("The word 'orange' is present in ("+a+") : " );
+		System.out.println(a.contains("orange"));
+	}
+}
