7ebaa8d3746dde729c61579a58132b62f9b55c18
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-16 23:40:01 +0530

    Create String11.java

---
+++ Java_Programs/String_programs_2/String11.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class inputSentence {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a sentence: ");
+		String a=sc.nextLine();
+		System.out.println("Entered sentence is: "+a);
+	}
+}
