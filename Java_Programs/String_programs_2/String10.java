84313069d22dc8b6a2301cbbf700bde188605274
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-16 23:34:18 +0530

    Create String10.java

---
+++ Java_Programs/String_programs_2/String10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+public class positionOfCharacter {
+
+	public static void main(String[] args) {
+		String a="Java Exercises!";
+		System.out.println("original string: "+a);
+		char b=  a.charAt(0);
+		char c=  a.charAt(10);
+	    System.out.println("The character at position 0 is "+b);
+	    System.out.println("The character at position 0 is "+c);
+    }
+}
