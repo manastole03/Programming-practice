35662706fe73b6c16f3de5ce8098d570be272dff
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-16 22:29:42 +0530

    Create String18.java

---
+++ Java_Programs/String_Programs1/String18.java
@@ -0,0 +1,14 @@
+package myAllJavaProjects;
+
+public class String18 
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Manas Tole";
+		
+		int i = str.hashCode();
+		System.out.print("Unique Identifier :" + i);
+	}
+
+}
