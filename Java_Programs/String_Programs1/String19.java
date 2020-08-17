b0f152144e82760877f03248d9776654356fb51c
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-17 23:24:12 +0530

    Create String19.java

---
+++ Java_Programs/String_Programs1/String19.java
@@ -0,0 +1,15 @@
+package myAllJavaProjects;
+
+public class String19
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Manas Tole";
+		int a = str.indexOf('M', 0);
+		int b = str.indexOf('a', 0);
+		System.out.print(a);
+		System.out.print("\n"+b);
+	}
+
+}
