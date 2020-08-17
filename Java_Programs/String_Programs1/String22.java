fe71f1dfe5b51284036d178ede2bbc799b323205
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-17 23:25:52 +0530

    Create String22.java

---
+++ Java_Programs/String_Programs1/String22.java
@@ -0,0 +1,11 @@
+package myAllJavaProjects;
+
+public class String22
+{
+	public static void main(String[] args)
+	{
+		String str = "Manas Tole";
+		System.out.print("Length of the string : " + str.length());
+
+	}
+}
