116d7509636e5b8436be498313c550d7024bd425
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-18 21:43:58 +0530

    Create String12.java

---
+++ Java_Programs/String_programs_2/String12.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+public class unicode {
+	public static void main(String[] args) {
+		String a="website.com";
+		System.out.println("Original string: "+a);
+		int v1=a.codePointAt(2);
+		int v2=a.codePointAt(1);
+		System.out.println("Charecter(unicode point)= "+v1);
+		System.out.println("Charecter(unicode point)= "+v2);
+	}
+}
