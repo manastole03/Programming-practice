8959714f320b917e52090b0691a29a840d4b4b56
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-18 22:29:24 +0530

    Create String18.java

---
+++ Java_Programs/String_programs_2/String18.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.Scanner;
+public class containsValues {
+	public static void main(String[] args) {
+		String a="PHP Exercise and Python Exercise";
+		System.out.println("String 1: "+a);
+		System.out.println("Specified sequence of char values(and)= "+a.contains("and"));
+	}
+}
