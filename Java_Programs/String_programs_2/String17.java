2b8063751d14aeb2da1ff95281bf8c827cdf91e2
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-18 22:23:17 +0530

    Create String17.java

---
+++ Java_Programs/String_programs_2/String17.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class concatStrings {
+	public static void main(String[] args) {
+		String a="PHP Exercise and";
+		String b="Python Exercise";
+		System.out.println("String 1: "+a);
+		System.out.println("String 2: "+b);
+		System.out.println("The concatenated string "+a.concat(" "+b));
+	}
+}
