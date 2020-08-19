bb3283a005f665c0b2c21ee5465d0a68d37b0c42
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-19 23:06:59 +0530

    Create String24.java

---
+++ Java_Programs/String_Programs1/String24.java
@@ -0,0 +1,12 @@
+package myAllJavaProjects;
+
+public class String24
+{
+	public static void main(String[] args)
+	{
+		String str = "Manas Tole";
+		
+		String my_string = str.replace('a' , 'c');
+		System.out.print("String :" + my_string);
+	}
+}
