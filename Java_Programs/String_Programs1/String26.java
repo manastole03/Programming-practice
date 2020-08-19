d2b14c7fc92f7d9f45de0ef651cc1052f56dd432
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-19 23:08:00 +0530

    Create String26.java

---
+++ Java_Programs/String_Programs1/String26.java
@@ -0,0 +1,14 @@
+package myAllJavaProjects;
+
+public class String26 
+{
+	public static void main(String[] args) 
+	{
+		String str = "Manas Tole";
+		
+		String str1 = "Manas";
+		
+		boolean result = str.startsWith(str1);
+		System.out.print("String starts with str1 :" + result);
+	}
+}
