0eb569321b39ed6f1002346b8751cbd8f2795257
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-19 22:32:56 +0530

    Create String23.java

---
+++ Java_Programs/String_programs_2/String23.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class endsWith {
+	public static void main(String[] args) {
+		String a="Java Exercise";
+    String d="Java Exercises";
+		String b="se";
+		System.out.println(a+" ends with ("+b+ ")? : "+a.endsWith(b));
+    System.out.println(d+" ends with ("+b+ ")? : "+d.endsWith(b));
+	}
+}
