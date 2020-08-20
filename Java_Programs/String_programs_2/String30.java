5573909b63a27ecc8fe420efed294a4d3b45d204
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-20 22:09:22 +0530

    Create String30.java

---
+++ Java_Programs/String_programs_2/String30.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class characterArray_to_String {
+
+	public static void main(String[] args) {
+		char[] a= {'h','e','l','l','o'};
+		String b=new String(a);
+		System.out.println(b);
+		
+	}
+}
