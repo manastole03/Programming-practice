06ba4506dd96a63a72aaab7a5a51edbcbd3f4d5e
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-20 21:52:42 +0530

    Create String28.java

---
+++ Java_Programs/String_programs_2/String28.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class byte_to_String {
+
+	public static void main(String[] args) {
+		byte b[]= {84, 104, 105, 115, 32, 105, 115, 32, 97, 32, 115, 97, 109, 112, 108, 101, 32, 115, 116, 114, 105, 110, 103};
+		String a=new String(b);
+		System.out.println(a);
+		
+	}
+}
