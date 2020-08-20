ee47b17636f3e524e27d947e298a7731618d20bb
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-20 21:50:41 +0530

    Create String27.java

---
+++ Java_Programs/String_programs_2/String27.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+public class string_to_byte {
+
+	public static void main(String[] args) {
+  String a="This is a sample string";
+		byte b[]=a.getBytes();
+		System.out.println(Arrays.toString(b));
+	}
+}
