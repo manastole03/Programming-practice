5bf18513d9780bee5094e0ee56498fb10975bafd
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-20 22:25:39 +0530

    Create String32.java

---
+++ Java_Programs/String_programs_2/String32.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+public class characterAtIndex {
+
+	public static void main(String[] args) {
+		String a="Python Exercises.";
+		for(int i=0;i<a.length();i++) {
+			System.out.println(i+"-"+a.charAt(i));
+		}
+		
+	}
+}
