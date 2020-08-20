2b22182d48c106dc573ec018692c6f093a9eab9c
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-20 22:19:28 +0530

    Create String31.java

---
+++ Java_Programs/String_programs_2/String31.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+public class hashCode {
+
+	public static void main(String[] args) {
+		String a="Python Exercises.";
+		int unique=a.hashCode();
+		System.out.println("The hash for ("+a+") is: "+unique);
+		
+		
+	}
+}
