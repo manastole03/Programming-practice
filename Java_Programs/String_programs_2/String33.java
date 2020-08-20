acd0bcb1734d2d5c80aac329609f16b84471bbf7
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-20 22:32:11 +0530

    Create String33.java

---
+++ Java_Programs/String_programs_2/String33.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+public class canonacial_of_String{
+
+	public static void main(String[] args) {
+		String a="hello";
+		String b="hello";
+		String c=new String("hello");
+		System.out.println(a==b);
+		System.out.println(a==c);
+		
+	}
+}
