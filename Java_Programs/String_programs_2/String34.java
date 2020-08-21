c6e81a9c62dd2d0ab51143739fa0beb687bfc191
sarvesha-jaiswal <66911278+sarvesha-jaiswal@users.noreply.github.com>
2020-08-21 18:53:23 +0530

    Create String34.java

---
+++ Java_Programs/String_programs_2/String34.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class selec {
+
+	public static void main(String[] args) {
+		char a[]=new char[]{1,2,3,4,5,6};
+		String b=String.copyValueOf(a);
+		System.out.println("the string: "+b+" is this ");
+		
+	}
+}
