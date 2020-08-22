0da7491c6b071de340aaaf20802ae6ce3a7fc5bd
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-22 22:58:14 +0530

    Create String30.java

---
+++ Java_Programs/String_Programs1/String30.java
@@ -0,0 +1,15 @@
+package myAllJavaProjects;
+import java.util.*;
+
+public class String30 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner (System.in);
+		
+		String str = sc.nextLine();
+		
+		String my = str.toUpperCase();
+		System.out.print("Upper-case string :" + my);
+	}
+}
