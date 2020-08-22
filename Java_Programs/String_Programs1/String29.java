78ce0f948586273156b61c29e3956f59cc645004
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-22 22:57:34 +0530

    Create String29.java

---
+++ Java_Programs/String_Programs1/String29.java
@@ -0,0 +1,12 @@
+package myAllJavaProjects;
+
+public class String29
+{
+	public static void main(String[] args) 
+	{
+		String str = "MANAS TOLE";
+		
+		String my = str.toLowerCase();
+		System.out.print("Lower-case string :" + my);
+	}
+}
