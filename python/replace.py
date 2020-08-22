72a5a86811521af143b1a91c1e29b3515ac967f8
Damayanti2911 <68653915+Damayanti2911@users.noreply.github.com>
2020-08-22 18:08:00 +0530

    Delete replace.py

--- python/replace.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to get a string from a given string where all occurrences of its first char have been changed to '$', except the first char itself. 
-
-
-inp=input('enter the string: ')
-char = inp[0]
-inp = inp.replace(char, '$')
-inp = char + inp[1:]
-print(inp)
