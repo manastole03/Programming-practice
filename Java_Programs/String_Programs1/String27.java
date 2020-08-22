4c5282f7869637f833db3f3d123570d9608f3573
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-22 22:56:27 +0530

    Create String27.java

---
+++ Java_Programs/String_Programs1/String27.java
@@ -0,0 +1,14 @@
+package myAllJavaProjects;
+
+public class String27
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Manas Tole";
+		
+		String my = str.substring(2, 7);
+		System.out.print("Substring :" + my);
+	}
+
+}
