5a3946e1bba338ec0b478bd88232d012063f2715
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-22 22:58:44 +0530

    Create String31.java

---
+++ Java_Programs/String_Programs1/String31.java
@@ -0,0 +1,13 @@
+package myAllJavaProjects;
+
+public class String31
+{
+	public static void main(String[] args)
+	{
+		String str = "Manas Tole";
+		
+		String my = str.trim();
+		System.out.print("Trimed String : " + my);
+
+	}
+}
