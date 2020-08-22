4ad62a770e5e17628fadd0f835a0b1b9ba494732
Manas Arun Tole <55608869+manastole03@users.noreply.github.com>
2020-08-22 22:56:54 +0530

    Create String28.java

---
+++ Java_Programs/String_Programs1/String28.java
@@ -0,0 +1,13 @@
+package myAllJavaProjects;
+
+public class String28
+{
+	public static void main(String[] args)
+	{
+		String str = "Manas Tole";
+		
+		char[] my = str.toCharArray();
+		
+		System.out.print("Char - array :" + my);
+	}
+}
